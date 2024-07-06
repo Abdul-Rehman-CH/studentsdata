0655c80cb86199a263f8e92e7b793ab6c3578a69
Abdul-Rehman-CH <Abdulrehmanc435@gmail.com>
2024-07-06 17:26:59 +0500

    New creativity

---
+++ script.js
@@ -0,0 +1,89 @@
+const Abdulrehman ={
+    name : "Abdulrehman",
+    age : "20",    
+    gender : "Male",
+    address : "Karachi",
+    phone : "0300-1234567",
+    email : "abdulrehman@gmail.com",
+}
+const Ahmad ={
+    name : "Ahmad",
+    age : "21",    
+    gender : "Male",
+    address : "Karachi",
+    phone : "0300-1234367",
+    email : "Ahmad@gmail.com",
+}
+const Umer ={
+    name : "Umer",
+    age : "22",    
+    gender : "Male",
+    address : "Karachi",
+    phone : "0300-1234567",
+    email : "Umer@gmail.com",
+}
+const Wania ={
+    name : "Wania",
+    age : "20",    
+    gender : "Female",
+    address : "Karachi",
+    phone : "0300-1234567",
+    email : "Wania@gmail.com",
+}
+const Laiba ={
+    name : "Laiba",
+    age : "19",    
+    gender : "Female",
+    address : "Karachi",
+    phone : "0300-1234457",
+    email : "Laiba@gmail.com",
+}
+const Saqlain ={
+    name : "Saqlain",
+    age : "25",    
+    gender : "Male",
+    address : "Karachi",
+    phone : "0300-1234567",
+    email : "Saqlain@gmail.com",
+}
+const Ayesha ={
+    name : "Ayesha",
+    age : "20",    
+    gender : "Female",
+    address : "Karachi",
+    phone : "0300-1234567",
+    email : "Ayesha@gmail.com",
+}
+const Umair ={
+    name : "Umair",
+    age : "20",    
+    gender : "Male",
+    address : "Karachi",
+    phone : "0300-1234567",
+    email : "Umair@gmail.com",
+}
+const Fatima ={
+    name : "Fatima",
+    age : "20",    
+    gender : "Female",
+    address : "Karachi",
+    phone : "0300-1234567",
+    email : "Fatima@gmail.com",
+}
+const Abdulhanan ={
+    name : "AbdulHanan",
+    age : "20",    
+    gender : "Male",
+    address : "Karachi",
+    phone : "0300-1234567",
+    email : "abdulhanan@gmail.com",
+}
+const Hassaan ={
+    name : "Hassaan",
+    age : "20",    
+    gender : "Male",
+    address : "Karachi",
+    phone : "0300-1234567",
+    email : "Hassaan@gmail.com",
+}
+console.log(Umair)
